73d0b202aba68d383aee522acb82e0a3602c695e
Bushra jabin <bushrabintjabin@gmail.com>
2024-02-05 19:04:20 +0530

    Routing remove

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from "react";
 import Login from "./components/loginForm/Login";
 import Signup from "./components/Signup/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WeatherComponent from "./components/weather/WeatherComponent";
 
 function App() {
   return (
     <>
     <Login/>
-    <WeatherComponent/>
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={Signup} />
